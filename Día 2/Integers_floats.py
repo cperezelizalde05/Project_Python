0b06b6d2e0e2208603bb079801b31f5128b6747b
Carlos Pérez Elizalde <128245186+cperezelizalde05@users.noreply.github.com>
2023-07-03 12:26:54 -0300

    New commit

---
+++ Día 2/Integers_floats.py
@@ -0,0 +1,20 @@
+#mi_numero= 1
+#mi_numero= 1 + 3
+#mi_numero= 5.8
+#mi_numero = 5.8 + 8
+#print(mi_numero + mi_numero)
+#print(mi_numero)
+#print(type(mi_numero))
+#mi_edad = input("Dime tu edad: ")
+#print("Tu edad es " + mi_edad)
+#nueva_edad = 1 + mi_edad
+#print("Vas a cumplir " + nueva_edad)
+#num_entero = 10
+#print(num_entero)
+
+num1 = 7.5
+num2 = 2.5
+suma = num1 + num2
+print(suma)
+
+print(type(suma))
---
+++ Día 2/Variables.py
@@ -0,0 +1,33 @@
+#nombre = "Juan"
+#print(nombre)
+
+#nombre = "Laura"
+#print(nombre)
+
+#edad = 30
+#print(edad)
+
+#edad2=35
+#print(edad+edad2)
+
+#nombre = input("Dime tu nombre ")
+#input("Tu nombre es " + nombre)
+
+#nombre1="hola "
+#nombre2="Python"
+#frase = nombre1 + nombre2
+#input(frase)
+
+#edad1=30
+#edad2=35
+#frase=edad1+edad2
+#input(frase)
+
+#nombre = "Julia "
+#apellido = "Roberts"
+#nombrecompleto = nombre + apellido
+#input(nombrecompleto)
+
+#curso="Python"
+#input("Estás tomando un curso de " + curso)
+
