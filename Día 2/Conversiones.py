7cb597f4f4ded277cee77fa6598dbe516e13ec96
Carlos Pérez Elizalde <128245186+cperezelizalde05@users.noreply.github.com>
2023-07-05 14:53:53 -0300

    New commit

---
+++ Día 2/Conversiones.py
@@ -0,0 +1,35 @@
+#Conversiones Implícitas
+#num1 = 20
+#num2 = 30.5
+
+#num1 = num1 + num2
+
+#print(type(num1))
+#print(type(num2))
+
+#Conversiones Explícitas
+
+#num1 = 5.8
+#print(num1)
+#print(type(num1))
+
+#num2 = int(num1)
+#print(num2)
+#print(type(num2))
+
+#edad = input("Dime tu edad: ")
+#edad = int(edad)
+#nueva_edad = 1 + edad
+#print(nueva_edad)
+
+
+
+
+#num1 = "7.5"
+#num2 = "10"
+#num3 = float(num1)
+#num4 = float(num2)
+#print(num3 + num4)
+
+
+
---
+++ Día 2/Formatear_Cadenas.py
@@ -0,0 +1,17 @@
+#Función Format
+#x = 10
+#y = 5
+
+#print("Mis números son {} y {}" .format(x,y))
+
+#print("La suma de {} y {} es igual a {}" .format(x,y,x+y))
+
+#Cadenas Literales
+#color = "rojo"
+#matricula = 541926
+#print(f"El auto es {color} y matricula es {matricula}")
+
+#puntos_anteriores = 875
+#puntos_nuevos = 350
+#puntos_totales = puntos_nuevos + puntos_anteriores
+#print(f"Has ganado {puntos_nuevos} puntos! En total, acumulas {puntos_totales}")
