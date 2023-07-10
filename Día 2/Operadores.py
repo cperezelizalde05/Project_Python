9505c57a1280ed5089ef2f3552c59fb13cabc83a
Carlos Pérez Elizalde <128245186+cperezelizalde05@users.noreply.github.com>
2023-07-10 16:07:23 -0300

    New commit

---
+++ Día 2/Operadores.py
@@ -0,0 +1,19 @@
+#Suma, resta, multiplicación y división
+x = 6
+y = 2
+z = 7
+
+print(f"{x} más {y} es igual a {x+y}")
+print(f"{x} menos {y} es igual a {x-y}")
+print(f"{x} por {y} es igual a {x*y}")
+print(f"{x} dividido {y} es igual a {x/y}")
+
+
+#División al piso: Es cuando divido un valor y lo redondea hacia abajo.
+print(f"{z} dividido {y} al piso es igual a {z//y}")
+
+#Módulo, es cuando por ejemplo divido 7/2, es 3,5. 0,5 sería el módulo
+print(f"{z} módulo de {y} es igual a {z%y}")
+print(f"{x} elevado al {y} es igual a {x**y}")
+print(f"{x} elevado al {3} es igual a {x**3}")
+print(f"La raíz cuadrada de {x} el resultado es {x**0.5}")
