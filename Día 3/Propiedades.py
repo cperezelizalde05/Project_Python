080a804e2c3d5a7769be69f524e8f88c7e92fffa
Carlos Pérez Elizalde <128245186+cperezelizalde05@users.noreply.github.com>
2023-07-20 15:45:41 -0300

    New commit

---
+++ Día 3/Propiedades.py
@@ -0,0 +1,11 @@
+n1 = "Kari"
+n2 = "na"
+print(n1 + n2)# concatenar
+print(n1* 10)# multiplicar
+poema = """Mil pequeños peces blancos
+como si hirviera
+el color del agua"""
+print(poema)
+print("agua"in poema)# búsqueda dentro del string
+print("sol" not in poema)
+print(len(poema))# permite saber el largo del string
