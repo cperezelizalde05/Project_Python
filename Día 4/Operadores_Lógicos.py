ea6d327d8bd800676008f0de23dd132bc4006258
Carlos Pérez Elizalde <128245186+cperezelizalde05@users.noreply.github.com>
2023-08-16 10:17:06 -0300

    New commit

---
+++ Día 4/Operadores_Lógicos.py
@@ -0,0 +1,19 @@
+mi_bool =  4 < 5 < 6
+print(mi_bool)
+
+mi_bool2 = 4 < 5 and 5 > 6
+print(mi_bool2)
+
+mi_boo3 = (55==55) and ("perro" == "perro")
+print(mi_boo3)
+
+mi_bool4 = 10 == 10 or 3 == 3 #si la primera fuera falsa y la segunda verdadera lo mismo da verdader porque se da una u otra condición
+print(mi_bool4)
+
+texto = "Esta frase es breve"
+mi_bool5 = ("frase" in texto) and ("breve" in texto)
+
+mi_bool6 = not ("a" == "a") #los paréntesis no son obligatorios
+print(mi_bool6)
+
+
---
+++ Día 4/Operadores_de_Comparación.py
@@ -0,0 +1,6 @@
+mi_bool = 10==25
+mi_bool2 = 5+5 == 8*2
+mi_bool3 = "blanco" == "Blanco".lower()
+print(mi_bool)
+print(mi_bool2)
+print(mi_bool3)
